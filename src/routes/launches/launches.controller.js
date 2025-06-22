ca3472cebf75fabdf31f0bb7f8f4ee978b88b3d5
Sohan Ghosh <isohan2007@gmail.com>
2025-06-22 18:41:58 +0530

    add error handling in launch endpoint

--- src/routes/launches/launches.controller.js
+++ src/routes/launches/launches.controller.js
@@ -5,12 +5,29 @@ function httpGetAllLaunches(req, res) {
   return res.status(200).json(getAllLaunches());
 }
 
-function httpAddNewLaunch(req,res){
-    const launch = req.body;
-    launch.launchDate = new Date(launch.launchDate)
+function httpAddNewLaunch(req, res) {
+  const launch = req.body;
 
-    const newLaunch = addNewLaunch(launch);
-    return res.status(201).json(newLaunch);
+  if (
+    !launch.mission ||
+    !launch.rocket ||
+    !launch.launchDate ||
+    !launch.destination
+  ) {
+    return res.status(400).json({
+      error: "Missing or invalid launch property",
+    });
+  }
+
+  launch.launchDate = new Date(launch.launchDate);
+  if (isNaN(launch.launchDate)) {
+    return res.status(400).json({
+      error: "Invalid launch date",
+    });
+  }
+
+  const newLaunch = addNewLaunch(launch);
+  return res.status(201).json(newLaunch);
 }
 
 module.exports = {
